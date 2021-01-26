4ef8af213d729d9bdff97abf01e1d09dddf6f237
burhancoulibaly <burhancoulibaly@gmail.com>
2021-01-26 23:48:30 +0000

    worked on rooms page, added inifinite scrolling

--- components/roomsContent.tsx
+++ components/roomsContent.tsx
@@ -1,10 +1,39 @@
 import Link from 'next/link';
 import '../styles/rooms.module.css';
 
-export default function Rooms({ auth, ...props }){
-
+function LiveRooms({ roomsState, ...props }){
+    const liveRooms = roomsState.rooms.map((room, index) => {
+        return <div key={index} className="live-room">{index}</div>
+    });
+    
     return (
         <>
+            {liveRooms}
+        </>
+    )
+}
+
+export default function RoomsContent({ auth, scrollBottom, roomsState, ...props }){
+    return (
+        <>
+            <div className="room-heading">
+                <h1>Rooms</h1>
+                <div className="room-options">
+                    <h1>Filter/Search Options Here</h1>
+                </div>
+            </div>
+
+            <div className="rooms-container">  
+                <LiveRooms
+                    roomsState={roomsState}
+                />
+            </div>
+
+            {roomsState.loading && 
+                <div className="loading-rooms">
+                    <h3>...Loading</h3>
+                </div>
+            }
         </>
     )
 }
--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -3,26 +3,121 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import '../styles/rooms.module.css';
 import { useAuthGuard } from '../hooks/useAuthGuard';
+import React, { useEffect, useState } from 'react';
+import RoomsContent from '../components/roomsContent';
 
-export default function Security({ ...props }){
+export default function Rooms({ ...props }){
     const auth = useAuthGuard();
     const router = useRouter();
+    const [scrollBottom, setScrollBottom] = useState({ isBottom: false });
+    const [roomsState, setRoomsState] = useState({
+        rooms: [
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+        ],
+        loading: false
+    })
+
+    // console.log(scrollBottom);
+
+    const getNewRooms = () => {
+        return [
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+            { roomId: "", roomname: "", users: {}, thumbnail: "", roomOwner: "" },
+        ]
+    }
+
+    const loadMoreRooms = async() => {
+        let newRooms;
+
+        setRoomsState({
+            ...roomsState,
+            loading: true
+        })
+
+        setTimeout(() => {
+            newRooms = roomsState.rooms.concat(getNewRooms());
+
+            setRoomsState({
+                ...roomsState,
+                loading: false
+            })
+    
+            setRoomsState({
+                ...roomsState,
+                rooms: newRooms
+            })
+        }, 1000)
+    }
+
+    useEffect(() => {
+        const handleWinScroll = () => {
+            // console.log(window.scrollY + window.innerHeight,  document.body.scrollHeight)
+            if((window.scrollY + window.innerHeight) >= document.body.scrollHeight){
+                setScrollBottom({
+                    ...scrollBottom,
+                    isBottom: ((window.scrollY + window.innerHeight) >= document.body.scrollHeight)
+                })
+            }else{
+                if(scrollBottom.isBottom){
+                    setScrollBottom({
+                        ...scrollBottom,
+                        isBottom: ((window.scrollY + window.innerHeight) >= document.body.scrollHeight)
+                    })
+                }
+            }
+        }
+
+        window.addEventListener('scroll', handleWinScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleWinScroll);
+        };
+    })
+
+    useEffect(() => {
+        if(scrollBottom.isBottom){
+            loadMoreRooms();
+        }
+    }, [scrollBottom])
 
     return (
         <>
-        { auth.user === 'loading' && 
-            <>
-            </>
-        }
-        { (auth.user !== 'loading' && (auth.user || Object.values(auth.user).length)) &&
-            <>
-                <Image src="/images/luke-chesser-3rWagdKBF7U-unsplash(1).jpg" unoptimized={false} alt="account-page-background" layout="fill" objectFit="cover"/>
-
-                <div id="rooms">
-
-                </div>
-            </>
-        }
+            <div id="rooms">
+                <RoomsContent
+                    auth={auth}
+                    scrollBottom={scrollBottom}
+                    roomsState={roomsState}
+                />
+            </div>
         </>
     )
 }
